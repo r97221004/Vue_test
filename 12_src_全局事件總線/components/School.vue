d45b66a6136a5f15d4ee6ec001e43629c0abbbd7
r97221004 <r97221004@ntu.edu.tw>
2022-12-27 18:44:13 +0800

    add mod12

---
+++ 12_src_全局事件總線/components/School.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="school">
+    <h2>學校名稱：{{ name }}</h2>
+    <h2>學校地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "尚矽谷",
+      address: "北京",
+    };
+  },
+  mounted() {
+    // console.log('School',this)
+    this.$bus.$on("hello", (data) => {
+      console.log("我是 School 組件，收到了數據", data);
+    });
+  },
+  beforeDestroy() {
+    this.$bus.$off("hello");
+  },
+};
+</script>
+
+<style scoped>
+.school {
+  background-color: skyblue;
+  padding: 5px;
+}
+</style>
---
+++ 12_src_全局事件總線/components/Student.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="student">
+    <h2>學生姓名：{{ name }}</h2>
+    <h2>學生性別：{{ sex }}</h2>
+    <button @click="sendStudentName">把學生名給 School 組件</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "張三",
+      sex: "男",
+    };
+  },
+  mounted() {
+    // console.log('Student',this.x)
+  },
+  methods: {
+    sendStudentName() {
+      this.$bus.$emit("hello", this.name);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.student {
+  background-color: pink;
+  padding: 5px;
+  margin-top: 30px;
+}
+</style>
