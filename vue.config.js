0038388b9af86c350dd1d842a63805296df784ae
r97221004 <r97221004@ntu.edu.tw>
2022-12-31 20:41:31 +0800

    add mod19

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false // 關閉語法檢查
+  lintOnSave: false, // 關閉語法檢查
+  //開啟代理服務器（方式一）
+  // devServer: {
+  //   proxy: 'http://127.0.0.1:5000'
+  // },
+  //開啟代理服务器（方式二）
+  devServer: {
+    proxy: {
+      '/atguigu': {
+        target: 'http://127.0.0.1:5000',
+        pathRewrite: { '^/atguigu': '' },
+        // ws: true, //用于支持 websocket
+        // changeOrigin: true //用于控制請求頭中的 host 值
+      },
+      '/demo': {
+        target: 'http://localhost:5001',
+        pathRewrite: { '^/demo': '' },
+        // ws: true, //用于支持 websocket
+        // changeOrigin: true //用于控制請頭中的 host 值
+      }
+    }
+  }
 })
