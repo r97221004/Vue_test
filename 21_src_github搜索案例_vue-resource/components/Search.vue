e31352080b846386d4acaa0748e40780bcb06dbc
r97221004 <r97221004@ntu.edu.tw>
2023-01-01 16:33:54 +0800

    add mod21

---
+++ 21_src_github搜索案例_vue-resource/components/Search.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="jumbotron">
+    <h3 class="jumbotron-heading">Search Github Users</h3>
+    <div>
+      <input
+        type="text"
+        placeholder="enter the name you search"
+        v-model="keyWord"
+      />&nbsp;
+      <button @click="searchUsers">Search</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      keyWord: "",
+    };
+  },
+  methods: {
+    searchUsers() {
+      //請求前更新 List 的數據
+      this.$bus.$emit("updateListData", {
+        isLoading: true,
+        errMsg: "",
+        users: [],
+        isFirst: false,
+      });
+      this.$http
+        .get(`https://api.github.com/search/users?q=${this.keyWord}`)
+        .then(
+          (response) => {
+            console.log("請求成功了");
+            //請求成功後更新 List 的數據
+            this.$bus.$emit("updateListData", {
+              isLoading: false,
+              errMsg: "",
+              users: response.data.items,
+            });
+          },
+          (error) => {
+            //請求後更新 List 的數據
+            this.$bus.$emit("updateListData", {
+              isLoading: false,
+              errMsg: error.message,
+              users: [],
+            });
+          }
+        );
+    },
+  },
+};
+</script>
