803daf977e6efa087cc64994901e9e27e2d87e6f
r97221004 <r97221004@ntu.edu.tw>
2023-01-07 15:03:28 +0800

    add mod32

---
+++ 32_src_路由的query參數/pages/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Home組件內容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link
+            class="list-group-item"
+            active-class="active"
+            to="/home/news"
+            >News</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            class="list-group-item"
+            active-class="active"
+            to="/home/message"
+            >Message</router-link
+          >
+        </li>
+      </ul>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  /* beforeDestroy() {
+			console.log('Home 組件即將被銷毀了')
+		}, */
+  /* mounted() {
+			console.log('Home 組件掛載完畢了',this)
+			window.homeRoute = this.$route
+			window.homeRouter = this.$router
+		},  */
+};
+</script>
