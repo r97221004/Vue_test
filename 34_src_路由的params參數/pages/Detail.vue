723e19f4678122918bf4a4df1d9f2faa419a077e
r97221004 <r97221004@ntu.edu.tw>
2023-01-07 15:46:13 +0800

    add mod34

---
+++ 34_src_路由的params參數/pages/Detail.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li>消息編號：{{ $route.params.id }}</li>
+    <li>消息標題：{{ $route.params.title }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  mounted() {
+    // console.log(this.$route)
+  },
+};
+</script>
