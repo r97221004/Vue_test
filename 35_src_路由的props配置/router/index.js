ae7d9638cda568d039109c76239532c4b6d92343
r97221004 <r97221004@ntu.edu.tw>
2023-01-07 16:35:01 +0800

    add mod35

---
+++ 35_src_路由的props配置/router/index.js
@@ -0,0 +1,57 @@
+// 該文件專門用於創建整個應用的路由器
+import VueRouter from 'vue-router'
+// 引入組件
+import About from '../pages/About'
+import Home from '../pages/Home'
+import News from '../pages/News'
+import Message from '../pages/Message'
+import Detail from '../pages/Detail'
+
+// 創建並暴露一個路由器
+export default new VueRouter({
+	routes: [
+		{
+			name: 'guanyu',
+			path: '/about',
+			component: About
+		},
+		{
+			path: '/home',
+			component: Home,
+			children: [
+				{
+					path: 'news',
+					component: News,
+				},
+				{
+					path: 'message',
+					component: Message,
+					children: [
+						{
+							name: 'xiangqing',
+							path: 'detail',
+							component: Detail,
+
+							//props 的第一種寫法，值為對象，該對象中的所有 key-value 都會以 props 的形式傳給 Detail 組件。
+							// props:{a:1,b:'hello'}
+
+							//props 的第二種寫法，值為布爾值，若布爾值為真，就會把該路由組件收到的所有 params 參數，以 props 的形式傳給 Detail 組件。
+							// props:true
+
+							//props 的第三種寫法，值為函數
+							props($route) {
+								return {
+									id: $route.query.id,
+									title: $route.query.title,
+									a: 1,
+									b: 'hello'
+								}
+							}
+
+						}
+					]
+				}
+			]
+		}
+	]
+})
