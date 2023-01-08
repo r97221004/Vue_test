f774dd94faed9ddac702bba0e5ccba7519cd8c17
r97221004 <r97221004@ntu.edu.tw>
2023-01-08 20:02:54 +0800

    add mod41

---
+++ 41_src_獨享路由守衛/main.js
@@ -0,0 +1,20 @@
+//引入 Vue
+import Vue from 'vue'
+//引入 App
+import App from './App.vue'
+//引入 VueRouter
+import VueRouter from 'vue-router'
+//引入路由器
+import router from './router'
+
+//關閉 Vue 的生產提示
+Vue.config.productionTip = false
+//應用插件
+Vue.use(VueRouter)
+
+//創建 vm
+new Vue({
+	el: '#app',
+	render: h => h(App),
+	router: router
+})
